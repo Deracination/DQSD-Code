3860de8a5b4c4429cea87a5adebc5ce1c191fd40
davidbau <davidbau@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2001-12-18 16:30:20 +0000

    merged calendar; upgraded search.nsi to version 1.90 makensis; migrated search.xml; build is more robust

--- trunk/dqsd/src/build.cmd
+++ trunk/dqsd/src/build.cmd
@@ -3,11 +3,13 @@
 REM For this build to work, both the VC tools and
 REM the NSIS tools need to be on your path.
 
+call vcvars32.bat
+
 pushd %~dp0
 
 pushd DQSDTools
 nmake -f "DQSDTools.mak" CFG="DQSDTools - Win32 Release MinDependency"
-copy ReleaseMinDependency\DQSDTools.dll ..\..
+copy /y ReleaseMinDependency\DQSDTools.dll ..\..
 popd
 
 pushd ..
