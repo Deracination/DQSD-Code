125b0835d2a69fcf9823ed895bed29a3627c32a6
davidbau <davidbau@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2001-12-23 00:36:30 +0000

    Incorporated Koen's changes and some calendar generalizations.

--- dqsd/preferences.js
+++ dqsd/preferences.js
@@ -8,6 +8,8 @@ clockright=true;     // clock is right-justified
 clockgray=false;     // true draws clock on gray background
 clocklongwidth=139;  // long form is used if this pixel width is exceeded
 
+launchmode=2;        // 0=use ie; 1=use default browser; 2=auto
+defaultsearch="goo"; // use google search
 defaultcal="yhocal"; // use yahoo calendar
 
 // Note: valid calendars include
