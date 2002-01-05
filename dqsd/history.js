bd8fa47abd0be681756da2e37bc03e6c8eae08fd
glenncarr <glenncarr@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2002-01-05 08:57:04 +0000

    mods to save history

--- dqsd/history.js
+++ dqsd/history.js
@@ -1,15 +1,45 @@
 // fancy UI: history and cut and paste (via arrow and control keys)
 
-histarray = new Array(historylength);
+histarray = new Array(historylength + 1);
 histedit = new Array(histarray.length);
 histarray[0] = "";
 histcount = 1;
 histcurr = 0;
 histend = 0;
 
+restoreHistory();
+
+function saveHistory()
+{
+  writeFile("history.txt", histarray.join('\n'));
+}
+
+function restoreHistory()
+{
+  var loaded = readFile( "history.txt" ).split('\n');
+  for (var i = 1; i < histarray.length; i++)
+  {
+    if (!loaded[i] || loaded[i] == "")
+      break;
+    histarray[i] = loaded[i];
+  }
+  histcount = histend = i;
+}
+
+function historyItemExists(t)
+{
+  for (var i = histend-1; i >= 0; i--)
+    if (t == histarray[i])
+      return true;
+  return false;
+}
+
 // add an item to the history
 function addhist(t)
 {
+  // Don't add if already exists
+  if (histend > 0 && historyItemExists(t))
+    return;
   var i;
   for (i = 0; i < histcount; i++)
     histedit[i] = null;
@@ -21,6 +51,8 @@ function addhist(t)
   histarray[histend] = "";
   if (histcount < histarray.length)
     histcount = histcount + 1;
+    
+  saveHistory();
 }
 
 // get the current history entry
