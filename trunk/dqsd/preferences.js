ed40f22a06f6361e19abd41c9629127b7ad6359e
mscroggi <mscroggi@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2002-02-14 05:07:14 +0000

    reformatted for greater readability - no definition changes

--- trunk/dqsd/preferences.js
+++ trunk/dqsd/preferences.js
@@ -1,61 +1,125 @@
-// Configuration for Dave's Quick Search Deskbar features.
-// After you've saved your changes, enter "!" in the deskbar to reload.
 
-clock=true;          // false disables the clock
-clockdelay=5000;     // clock shows after 5 secs
-clockupdate=1000;    // clock updates every 1 sec
-clocklongwidth=139;  // long form is used if this pixel width is exceeded
+/* Configuration for Dave's Quick Search Deskbar features.
+*  After you've saved your changes, enter "!" in the deskbar 
+*  to reload.
+*/
 
-//reuseBrowserWindowMode = 2; // 0=new window always [default]; 1=single window for all; 2=single window for each search type
-launchmode=2;        // 0=use ie; 1=use default browser; 2=auto
-//language="en";    // uncomment to override default language
-cal=true;            // right-click shows popup calendar
-//eventsfileurl="events.xml"; //default xml file for the popup calendar events
-//multiline=false;   // default=true; set this to false to prevent word wraps
-
-defaultcal="yhocal"; // click on a calendar day uses yahoo calendar
-// Note: valid calendars include
-// yhocal - Yahoo Calendar
-// msncal - MSN Calendar
-// aolcal - AOL Calendar
-// nsccal - Netscape Calendar
-// mycal  - MyCalendar.com
-// evcal  - Evite.com
+/* Display the clock?
+*  true = display
+*  false = do not display
+*/
+clock=true;          
 
 
-// The clock is formatted according to the following format strings:
+/* clock display delay in seconds
+*  every 1000=1 second ex: 5000=5 seconds
+*/
+clockdelay=5000;
 
+
+/* clock update rate in seconds
+*  every 1000=1 second
+*/
+clockupdate=1000;
+
+/* clock long form width.  
+*  long form is used if this pixel width is exceeded
+*/
+clocklongwidth=139;
+
+
+/* Reuse browser window mode
+*  0=new window always [default]
+*  1=single window for all
+*  2=single window for each search type
+*/
+//reuseBrowserWindowMode = 2;
+
+
+/* Browser launch mode
+*  0=use IE
+*  1=use default browser
+*  2=auto (launches in IE directly if IE is default browser)
+*/
+launchmode=2;
+
+
+/* Default language - uncomment to override default language */
+// language="en";
+
+
+/* Default search engine
+*  gg=google (default)
+*/
+//defaultsearch="gg";
+
+
+/* Display the calendar on right-click?
+*  true = display calendar (default)
+*  false = do not display
+*/
+cal=true;
+
+
+/* Events file for the popup calendar events 
+* holidays.us.xml - United States
+* holidays.ca.xml - Canada
+* holidays.sg.xml - Singapore
+*/
+eventsfileurl="holidays.us.xml";
+
+
+/* Use multi-line searchbar?  Set this to false to prevent word wraps.
+*  true = display multiline (default)
+*  false = do not display multiline
+*/
+//multiline=false;
+
+
+/* Default calendar. clicking on a calendar day will spawn the defined online calendar. 
+*  Valid calendars include:
+*  yhocal - Yahoo Calendar
+*  msncal - MSN Calendar
+*  aolcal - AOL Calendar
+*  nsccal - Netscape Calendar
+*  mycal  - MyCalendar.com
+*  evcal  - Evite.com
+*/
+defaultcal="yhocal";
+
+
+/* The clock is formatted according to the following format strings:
+*  yyyy - four digit year (2001)
+*  yy   - two digit year (01)
+*  MMMM - fully spelled month name (January)
+*  MMM  - abbreviated month name (Jan)
+*  MM   - zero-padded month number (01)
+*  M    - month number (1)
+*  dd   - zero-padded day-of-month number (01)
+*  d    - day-of-month number (1)
+*  hh   - zero-padded 12-hour clock hour (01)
+*  h    - 12-hour clock hour (1)
+*  HH   - zero-padded 24-hour clock hour (01, 13)
+*  H    - 24-hour clock hour (13)
+*  mm   - zero-padded minutes value (00)
+*  ss   - zero-padded seconds value (00)
+*  a    - 12-hour clock am/pm lowercase (am)
+*  A    - 12-hour clock am/pm uppercase (AM)
+*  EEEE - day-of-week full name (Monday)
+*  EEE  - day-of-week abbreviation (Mon)
+*/
 clocklongform=" EEE d MMM yyyy h:mm A ";
 clockshortform=" h:mm A ";
-clocktooltipform=null; // tooltip turned off by default
 
 
-/* show the menu popup button?
- *   0 = never
- *   1 = always
- *   2 = if search bar width is greater than clocklongwidth [DEFAULT]
+/* Display clock tooltip text  turned off by default*/
+clocktooltipform=true;
+
+
+/*  Show the menu '>>' popup button?
+ *  0 = never
+ *  1 = always
+ *  2 = if search bar width is greater than clocklongwidth (default)
  */
 showbutton = 2;
 
-/*
-Some useful formatting codes:
-
-yyyy - four digit year (2001)
-yy   - two digit year (01)
-MMMM - fully spelled month name (January)
-MMM  - abbreviated month name (Jan)
-MM   - zero-padded month number (01)
-M    - month number (1)
-dd   - zero-padded day-of-month number (01)
-d    - day-of-month number (1)
-hh   - zero-padded 12-hour clock hour (01)
-h    - 12-hour clock hour (1)
-HH   - zero-padded 24-hour clock hour (01, 13)
-H    - 24-hour clock hour (13)
-mm   - zero-padded minutes value (00)
-ss   - zero-padded seconds value (00)
-a    - 12-hour clock am/pm lowercase (am)
-A    - 12-hour clock am/pm uppercase (AM)
-EEEE - day-of-week full name (Monday)
-EEE  - day-of-week abbreviation (Mon)
-*/
