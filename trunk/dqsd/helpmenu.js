568b7eafbfb386fc74983dcf5dc49419d0d87994
glenncarr <glenncarr@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2002-05-21 03:48:06 +0000

    removed obsolete popup menu code

--- trunk/dqsd/helpmenu.js
+++ trunk/dqsd/helpmenu.js
@@ -1,79 +1,48 @@
-// code to take advantage of IE 5.5 popup window
-
-function mi(name, func, sym)
+function showpop()
 {
-  popheight += 18;
-  qsym = sym;
-  if (sym == "\\")
-    qsym = "\\\\";
-  return "<tr class=menuRow " +
-    " onmouseover=\"this.className='menuRowHigh';\"" +
-    " onmouseout=\"this.className='menuRow';\"" +
-    " onmouseup=\"this.className='menuRow';parent.mnu('" + func + "', &quot;" + qsym + "&quot;);\"" +
-    " >" +
-    "<td nowrap class=menuCommands>" +
-    name +
-    "</td><td nowrap class=menuAlias>" + sym + "</td></tr>";
-}
-
-function dv()
-{
-  if (shrink) return "";
-  popheight += 16;
-  return "<tr><td colspan=2 class=menuSeparator><hr class=menuHR></td></tr>";
-}
-
-function buildpop()
-{
-  pophelp = window.createPopup();
-  popheight = 5;
-  popwidth = 240;
-  shrink = (window.screen.height <= 700);
-  var menucode = "<table cellpadding=0 cellspacing=0 width=100%><tr><td valign=top><style>" + convertStylesToInline() + "</style>" +
-      "<table cellpadding=0 cellspacing=0 width=100%>" +
-      mi("About This Toolbar", "about", "");
-  var i;
-  var div = false;
-
-  //for (i = 0; i < menuarray.length; i++)
-  for (i = menuarray.length - 1; i >= 0; i--)
+  document.deff.q.focus();
+  document.deff.q.createTextRange().select();
+  var mb = new ActiveXObject("DQSDTools.MenuBuilder");
+  mb.AppendMenuItem( local(ABOUT_TEXT), "about" );
+  mb.AppendSeparator( 0 );
+  for (i = 0; i < categoryarray.length; i++)
   {
-    var entry = menuarray[i];
-    if (entry == "separator")
+    var categoryName = categoryarray[i];
+    var helparray = categories[categoryName];
+    
+    // Are there any enabled searches in this category?
+    for (var k = 0; k < helparray.length && !helparray[k].enabled; k++)
     {
-       if (!div) // don't add consecutive separators
-       {
-         menucode += dv();
-         div = true;
-       }
     }
-    else
+
+    // If all the searches are disabled in the category, don't add a submenu
+    if ( k == helparray.length )
+      continue;
+
+    var hsubmenu = mb.AppendSubMenu( categoryName );
+    helparray.sort( searchCompare );
+    for (var k = 0; k < helparray.length; k++)
     {
-       var search = searches[aliases[entry]];
-       if (search)
-       {
-         menucode += mi(search.name, search.fname, entry);
-         div = false;
-       }
+      var search = helparray[k];
+      var alias = getSearchAliases( search );
+      if ( search.enabled )
+        mb.AppendMenuItem( search.name + '\t' + alias.replace(/&/g, '&&'), search.aliases[0], hsubmenu );
     }
   }
-  menucode += "</table></td><tr></table>";
+  
+  appendMRUMenuSelections( mb );
 
-  pophelp.document.body.innerHTML = menucode;
-  pophelp.document.body.style.border="outset 2px";
-  pophelp.document.body.style.background='menu';
-}
-
-pophelp = null;
-pophelpisopen=false;
-
-function watchpopup(popvarname)
-{
-  var isopen = eval(popvarname + "isopen = " + popvarname + ".isOpen");
-  if (isopen)
+  var fn = mb.Display();
+  if ( fn )
   {
-    window.setTimeout("watchpopup('" + popvarname + "');", 100);
+    var alias = null;
+    if ( isInternalSearch( fn ) )
+      alias = searches[fn].aliases[1];
+    mnu( fn, alias );
+    if ( fn != 'about' )
+      saveMenuHistory( alias ? alias : fn );
   }
+  return false;
 }
 
 function saveMenuHistory( alias )
@@ -140,56 +109,3 @@ function appendMRUMenuSelections( mb )
   }
   
 }
-
-function showpop()
-{
-  document.deff.q.focus();
-  document.deff.q.createTextRange().select();
-  var mb = new ActiveXObject("DQSDTools.MenuBuilder");
-  mb.AppendMenuItem( "About DQSD...", "about" );
-  mb.AppendSeparator( 0 );
-  for (i = 0; i < categoryarray.length; i++)
-  {
-    var categoryName = categoryarray[i];
-    var helparray = categories[categoryName];
-    
-    // Are there any enabled searches in this category?
-    for (var k = 0; k < helparray.length && !helparray[k].enabled; k++)
-    {
-    }
-
-    // If all the searches are disabled in the category, don't add a submenu
-    if ( k == helparray.length )
-      continue;
-
-    var hsubmenu = mb.AppendSubMenu( categoryName );
-    helparray.sort( searchCompare );
-    for (var k = 0; k < helparray.length; k++)
-    {
-      var search = helparray[k];
-      var alias = getSearchAliases( search );
-      if ( search.enabled )
-        mb.AppendMenuItem( search.name + '\t' + alias.replace(/&/g, '&&'), search.aliases[0], hsubmenu );
-    }
-  }
-  
-  appendMRUMenuSelections( mb );
-
-  var fn = mb.Display();
-  if ( fn )
-  {
-    var alias = null;
-    if ( isInternalSearch( fn ) )
-      alias = searches[fn].aliases[1];
-    mnu( fn, alias );
-    if ( fn != 'about' )
-      saveMenuHistory( alias ? alias : fn );
-  }
-  return false;
-}
-
-function queuefocus()
-{
-  document.deff.q.focus();
-}
-
