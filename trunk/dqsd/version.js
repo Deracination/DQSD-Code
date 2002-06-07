db4cad7a2e0764421d800e984a16c169ff33d3f4
glenncarr <glenncarr@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2002-06-07 15:05:42 +0000

    Removed tabs and changed indentation

--- trunk/dqsd/version.js
+++ trunk/dqsd/version.js
@@ -1,47 +1,47 @@
-	function versionCheck()
-	{
-		var bSuccess = true;
-		var testObject;
+function versionCheck()
+{
+  var bSuccess = true;
+  var testObject;
 
-		// First, see if we can create the object at all
-		try
-		{
-			testObject = new ActiveXObject("DQSDTools.Launcher");
-		}
-		catch(e)
-		{
-			alert("The DQSD helper DLL is not correctly installed");
-			bSuccess = false;
-		}
+  // First, see if we can create the object at all
+  try
+  {
+    testObject = new ActiveXObject("DQSDTools.Launcher");
+  }
+  catch(e)
+  {
+    alert("The DQSD helper DLL is not correctly installed");
+    bSuccess = false;
+  }
 
-		// Try a version query on it
-		if(bSuccess)
-		{
-			try
-			{
-				if(!testObject.VersionIsCorrect(2,5,8,9))
-				{
-					throw "DQSDTools out of date";
-				}
-			}
-			catch(e)
-			{
-				alert("The DQSD helper DLL is out of date");
-				bSuccess = false;
-			}
-		}
-		testObject = null;
+  // Try a version query on it
+  if(bSuccess)
+  {
+    try
+    {
+      if(!testObject.VersionIsCorrect(2,5,8,9))
+      {
+        throw "DQSDTools out of date";
+      }
+    }
+    catch(e)
+    {
+      alert("The DQSD helper DLL is out of date");
+      bSuccess = false;
+    }
+  }
+  testObject = null;
 
 
-		return bSuccess;
-	}
+  return bSuccess;
+}
 
-	// Check for the correct ActiveX DLL version
-	if(!versionCheck())
-	{
-		// This is not a nice thing to do, but does reinforce the fact that you
-		// can't use the bar like it is
-		// Unfortunately, you can't do a toolbar refresh to recover from this
-		// so it might not be a great idea
-		window.close();
-	}
+// Check for the correct ActiveX DLL version
+if(!versionCheck())
+{
+  // This is not a nice thing to do, but does reinforce the fact that you
+  // can't use the bar like it is
+  // Unfortunately, you can't do a toolbar refresh to recover from this
+  // so it might not be a great idea
+  window.close();
+}
