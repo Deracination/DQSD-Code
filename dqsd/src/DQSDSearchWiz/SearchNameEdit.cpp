3dc6f390d56f7c7a0879772c0e258f6101edd246
glenncarr <glenncarr@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2002-09-01 13:08:41 +0000

    * Display outerHTML for FORM elements instead of innerHTML * Added <copyright> to search files * Fixed bugs causing incorrect URLs for the <LINK> and the form 'action'

--- dqsd/src/DQSDSearchWiz/SearchNameEdit.cpp
+++ dqsd/src/DQSDSearchWiz/SearchNameEdit.cpp
@@ -8,8 +8,15 @@
 
 LRESULT CSearchNameEdit::OnChar( UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
 {
-	if ( _tcschr( _T(";!@#%^&*()=+ \t~`/\\,<>:'\"[]{}|"), wParam ) )
-		return 0;
+	try
+	{
+		if ( _tcschr( _T(";!@#%^&*()=+ \t~`/\\,<>:'\"[]{}|"), wParam ) )
+			return 0;
+	}
+	catch ( ... )
+	{
+		MessageBox( _T("An enternal error was encountered:  CSearchNameEdit::OnChar"), _T("DQSD Search Wizard Exception"), MB_OK|MB_ICONERROR );
+	}
 
 	return DefWindowProc();
 }
