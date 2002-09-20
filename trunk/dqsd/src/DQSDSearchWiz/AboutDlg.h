baab541961f8f5d7ebdf02d0d75521c3016fff03
glenncarr <glenncarr@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2002-09-20 04:43:48 +0000

    in work

--- trunk/dqsd/src/DQSDSearchWiz/AboutDlg.h
+++ trunk/dqsd/src/DQSDSearchWiz/AboutDlg.h
@@ -10,7 +10,7 @@
 /////////////////////////////////////////////////////////////////////////////
 // CAboutDlg
 class CAboutDlg : 
-	public CAxDialogImpl<CAboutDlg>
+	public CDialogImpl<CAboutDlg>
 {
 public:
 	CAboutDlg()
@@ -27,6 +27,7 @@ BEGIN_MSG_MAP(CAboutDlg)
 	MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
 	COMMAND_ID_HANDLER(IDOK, OnOK)
 	COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
+	MESSAGE_RANGE_HANDLER(WM_MOUSEFIRST, WM_MOUSELAST, OnMouseMessage)
 END_MSG_MAP()
 // Handler prototypes:
 //  LRESULT MessageHandler(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
@@ -73,6 +74,14 @@ END_MSG_MAP()
 		m_hlAboutURL.SetHyperLink( csURL );
 		m_hlAboutURL.SubclassWindow( GetDlgItem( IDC_Caption ) );
 
+		// create a tool tip
+		m_tip.Create(m_hWnd);
+		ATLASSERT(m_tip.IsWindow());
+		m_tip.Activate(TRUE);
+
+		m_tip.AddTool( GetDlgItem( IDOK ), _T("this is the OK button") );
+		m_tip.AddTool( GetDlgItem( IDC_ChangeHistory ), _T("this is the change history") );
+
 		return 1;  // Let the system set the focus
 	}
 
@@ -88,8 +97,32 @@ END_MSG_MAP()
 		return 0;
 	}
 
+    // Override to change message filtering
+/*    virtual BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		switch(pMsg->message)
+		{
+		case WM_LBUTTONDOWN: 
+		case WM_LBUTTONUP: 
+		case WM_MOUSEMOVE:
+			m_tip.RelayEvent(pMsg);
+		}
+		return CDialogImplBase<CAboutDlg>::PreTranslateMessage(pMsg);
+	}
+*/
+	
+	LRESULT OnMouseMessage(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		MSG msg = { m_hWnd, uMsg, wParam, lParam };
+		if(m_tip.IsWindow())
+			m_tip.RelayEvent(&msg);
+		bHandled = FALSE;
+		return 1;
+	}
+
 private:
 	CHyperLink m_hlAboutURL;
+	CToolTipCtrl m_tip;
 
 };
 
