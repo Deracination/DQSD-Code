e578515ce9ac0c9e9d034a39e3634ea4e7ba8536
mscroggi <mscroggi@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2002-09-25 17:21:20 +0000

    changed css styles, modified the function name to be a hyperlink to the <link> text in the search.

--- trunk/dqsdweb/searchtoc.pl
+++ trunk/dqsdweb/searchtoc.pl
@@ -25,62 +25,22 @@ print q{
 <html>
 <head>
 <title>DQSD Searches</title>
+<link rel='stylesheet' type='text/css' href='http://cvs.dqsd.sourceforge.net/cgi-bin/viewcvs.cgi/*checkout*/dqsd/dqsd/search.css?rev=HEAD&content-type=text/plain'>
 <style>
-body,tr,td
-{
-   font-family: Verdana; 
-   font-size: x-small;
-   padding-left:4px;
-}
-table 
-{
-   vertical-align: top;
-   border: 1px inset;
-   border-collapse:collapse;
-   border-spacing:0;
-}
 th
 {
-   background: #fcfcfc;
-   color: #000050;
    text-align: left;
    font-weight: bold;
    border: 1px outset;
-   padding-left:4px;
+   padding-left:2px;
 }
-tr 
+td
 {
-   background: #fcfcfc;
-   color: #206090;
    vertical-align:top;
-   border: 0px;
-}
-a {
-	text-decoration: none;
-   padding-right: 4px;
-}
-a:hover {
-	text-decoration: underline;
-   padding-right: 4px;
-}
-
-table.helpboxDescTable,tr
-{
-   border-width:0px;
-   border-collapse:collapse;
-   border-spacing:0;
-   color:#206090;
-   vertical-align:top;
-   padding: 0px;
-}
-.helpboxDescLabels
-{
-   color:#206090;
 }
 .funcName
 {
    color: #600000;
-   padding-right: 4px;
 }
 .description
 {
@@ -92,18 +52,7 @@ table.helpboxDescTable,tr
 {
    color: #a07040;
    padding-right: 4px;
-}
-.categorylink 
-{
-   font-size: x-small;
-   text-decoration: none;
-}
-.lineNumbers
-{
-   background: #fcfcfc;
-   color: #000000;
-   padding-left: 4px;
-   padding-right: 4px;
+   vertical-align:top;
 }
 </style>
 </head>
@@ -119,16 +68,16 @@ closedir searchdir;
 my (@html,@categories);
 #generate a record for each xml file
 foreach $datafile (@datafiles) {
-   my ($name,$desctext,$category)=();
+   my ($name,$desctext,$category,$link)=();
    my $filedata=read_file("$searchdir/$datafile");
    for ($t=0; $t<scalar(@$filedata); $t++) {
       #----------------------------------------------
-      #trigger if the line contains the name tags
+      #trigger if the line contains the <name> tags
       if (@$filedata[$t]=~/<name.+\<\/name/i) {
          $name=(split(/<*\/*name>*/i,"@$filedata[$t]"))[1];
          }#if
       #----------------------------------------------
-      #trigger if the line contains the <description tag
+      #trigger if the line contains the <description> tag
       if (@$filedata[$t]=~/\<description/i) {
          if (@$filedata[$t]=~/\/description *>/i) {
             $desctext=(split(/<*\/*description>*/i,"@$filedata[$t]"))[1];
@@ -148,8 +97,15 @@ foreach $datafile (@datafiles) {
          push(@categories,"$category");
          }#if
 
+      #----------------------------------------------
+      #trigger if the line contains the link tags
+      if (@$filedata[$t]=~/<link.+\<\/link/i) {
+         $link=(split(/<*\/*link>*/i,"@$filedata[$t]"))[1];
+         $link="<a href=\"$link\"</a>";
+         }#if
+         
       }#for
-   push(@html,"<td class=\"category\"><a name=\"$category\">$category</a></td>\n<td><a href=\"http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/*checkout*/dqsd/dqsd/searches/$datafile?rev=HEAD&content-type=text/xml\">$datafile</a></td>\n<td class=\"funcName\">$name</td>\n<td class=description>$desctext<br><br>\n</td>\n</tr>");
+   push(@html,"<td class=\"category\"><a name=\"$category\">$category</a></td>\n<td><a href=\"http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/*checkout*/dqsd/dqsd/searches/$datafile?rev=HEAD&content-type=text/xml\">$datafile</a></td>\n<td class=\"funcNamehelpboxCommands\">$link$name</td>\n<td class=description>$desctext<br>\n</td>\n</tr>");
    }#foreach
 
 #sort by the categories
@@ -161,12 +117,12 @@ foreach $datafile (@datafiles) {
 $categorylink="";
 for ($t=0; $t<=scalar(@categories); $t++) {
    next if ($categories[$t]=~/^ *$/);
-   $categorylink .="\n<a class=\"categorylink\" href=\"\#$categories[$t]\">$categories[$t]</a>";
+   $categorylink .="\n<a class=\"helpboxLink\" href=\"\#$categories[$t]\">$categories[$t]</a>";
    }
 print "<center><h3>DQSD Searches</h3>$categorylink";
 print "<table>\n<th></th><th>Category</th><th>File</th><th>Name</th><th>Description</th>";
 for ($t=1; $t<=scalar(@html); $t++) {
-   print "\n<tr>\n<td class=\"lineNumbers\">$t</td>$html[$t-1]";
+   print "\n<tr>\n<td class=\"body\">$t</td>\n$html[$t-1]";
    }
    
 #end html
