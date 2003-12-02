72f007d895503cdb6ab2abc8a01a8ba86b163f7f
kimgrasman <kimgrasman@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2003-12-02 21:27:44 +0000

    * Changed so it adds the path to MSVC and makensis

--- trunk/dqsd/src/buildwiz.cmd
+++ trunk/dqsd/src/buildwiz.cmd
@@ -3,7 +3,11 @@
 REM For this build to work, both the VC tools and
 REM the NSIS tools need to be on your path.
 
-REM call vcvars32.bat
+REM This may not be needed for your environment
+call vcvars32.bat
+
+REM This is the path to NSIS, may be different or unnecessary for your environment
+SET PATH=%PATH%;"C:\Program Files\NSIS"
 
 pushd %~dp0
 
