874d71f8f41ccb094c50d0c5dbda17537823ad3c
kimgrasman <kimgrasman@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2003-12-17 20:28:50 +0000

    * Now builds installer with bz2-compression

--- dqsd/src/build.cmd
+++ dqsd/src/build.cmd
@@ -14,6 +14,7 @@ nmake -f "DQSDTools.mak" CFG="DQSDTools - Win32 Release MinDependency"
 copy /y ReleaseMinDependency\DQSDTools.dll ..\..\DQSDTools.dll
 popd
 
-makensis search.nsi
+REM NSIS-compile with bz2 compression - saves 120K off installer size
+makensis.exe /X"SetCompressor bzip2" search.nsi
 
 popd
