14ac1fcb16f9aa79b0ab76a127e55ef4fed36cc3
bearbr <bearbr@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2006-07-11 18:15:54 +0000

    updated clean.cmd to clean DQSDHost too

--- trunk/dqsd/src/clean.cmd
+++ trunk/dqsd/src/clean.cmd
@@ -5,8 +5,6 @@ REM Please don't check into cvs any files that are deleted by this script.
 
 pushd %~dp0
 
-
-
 pushd DQSDTools
 if exist DQSDTools.h del /q DQSDTools.h
 if exist DQSDTools.ncb del /q DQSDTools.ncb
@@ -17,6 +15,22 @@ if exist DQSDTools_i.c del /q DQSDTools_i.c
 if exist DQSDTools_p.c del /q DQSDTools_p.c
 if exist dlldata.c del /q dlldata.c
 if exist Debug rmdir /q /s Debug
+if exist ReleaseMinDependency rmdir /q /s ReleaseMinDependency
+popd
+
+pushd DQSDHost
+if exist DQSDHost.h del /q DQSDHost.h
+if exist DQSDHost.vcproj.vspscc del /q DQSDHost.vcproj.vspscc
+if exist DQSDHost.suo del /q /ah DQSDHost.suo
+if exist DQSDHost.ncb del /q DQSDHost.ncb
+if exist DQSDHost.opt del /q DQSDHost.opt
+if exist DQSDHost.plg del /q DQSDHost.plg
+if exist DQSDHost.tlb del /q DQSDHost.tlb
+if exist DQSDHost_i.c del /q DQSDHost_i.c
+if exist DQSDHost_p.c del /q DQSDHost_p.c
+if exist dlldata.c del /q dlldata.c
+if exist Debug rmdir /q /s Debug
+if exist Release rmdir /q /s Release
 if exist ReleaseMinDependency rmdir /q /s ReleaseMinDependency
 popd
 
