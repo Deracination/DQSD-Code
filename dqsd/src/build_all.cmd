e3b2ea21181067b7e543e8a864e07a441771e743
kimgrasman <kimgrasman@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2008-07-30 20:01:35 +0000

    * Added build_all.cmd, which builds both 32- and 64-bit packages * Merged clean.cmd into scrub.cmd and fixed up source cleaning to remove everything

---
+++ dqsd/src/build_all.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+SETLOCAL
+
+PUSHD %~dp0
+
+REM Get rid of any remnant build artefacts
+CALL scrub.cmd
+
+REM Build 32-bit first and 64-bit second, because the 64-bit 
+REM build process is smart enough to exclude all 32-bit artefacts
+CALL build.cmd
+CALL build64.cmd
+
+POPD
+
+ENDLOCAL
--- dqsd/src/scrub.cmd
+++ dqsd/src/scrub.cmd
@@ -7,12 +7,29 @@ pushd %~dp0
 
 pushd ..
 if exist dqsd.exe del dqsd.exe
+if exist dqsdx64.zip del dqsdx64.zip
 if exist DQSDTools.dll del DQSDTools.dll
-if exist dqsdt253.dll del dqsdt253.dll
-if exist dqsdt254.dll del dqsdt254.dll
-if exist dqsdt255.dll del dqsdt255.dll
+if exist DQSDTools64.dll del DQSDTools64.dll
 popd
 
-call .\clean.cmd
+REM Clean temporary files from source tree
+pushd DQSDTools
+if exist DQSDTools.h del /q DQSDTools.h
+if exist DQSDTools*.ncb del /q DQSDTools*.ncb
+if exist DQSDTools.opt del /q DQSDTools.opt
+if exist DQSDTools.plg del /q DQSDTools.plg
+if exist DQSDTools.tlb del /q DQSDTools.tlb
+if exist DQSDTools_i.c del /q DQSDTools_i.c
+if exist DQSDTools_p.c del /q DQSDTools_p.c
+if exist dlldata.c del /q dlldata.c
+if exist DQSDTools*.suo del /q DQSDTools*.suo
+if exist DQSDTools*.user del /q DQSDTools*.user
+
+if exist Debug rmdir /q /s Debug
+if exist Release rmdir /q /s Release
+if exist Win32 rmdir /q /s Win32
+if exist x64 rmdir /q /s x64
+popd
+
 
 popd
