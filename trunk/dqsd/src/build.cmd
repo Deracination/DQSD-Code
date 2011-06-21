3c3b31e6447b6e879a34eb54511e8046d0ae312f
kimgrasman <kimgrasman@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2011-06-21 19:47:30 +0000

    Updated to build both 32 and 64 bit with VS2008

--- trunk/dqsd/src/build.cmd
+++ trunk/dqsd/src/build.cmd
@@ -1,22 +1,16 @@
 @echo off
 
 REM Customize these paths to your local install
-set VSINSTALLDIR=C:\Program Files\Microsoft Visual Studio .NET 2003
 set NSISINSTALLDIR=C:\Program Files\NSIS
-set SDKINCLUDE=C:\Program Files\Microsoft SDKs\Windows\2003SP1\include
-set SDKLIB=C:\Program Files\Microsoft SDKs\Windows\2003SP1\lib
 
 REM **** Build DQSD DLLs ****
-call "%VSINSTALLDIR%\Common7\Tools\vsvars32.bat"
-
-set INCLUDE=%SDKINCLUDE%;%INCLUDE%
-set LIB=%SDKLIB%;%LIB%
+call "%PROGRAMFILES%\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 
 pushd %~dp0
 
 pushd DQSDTools
-devenv /useenv DQSDTools.sln /rebuild Release
-copy /y Release\DQSDTools.dll ..\..\DQSDTools.dll
+DEVENV DQSDTools.vs2008.sln /rebuild "Release|Win32"
+copy /y Win32\Release\DQSDTools.dll ..\..\DQSDTools.dll
 popd
 
 REM **** Build installer ****
