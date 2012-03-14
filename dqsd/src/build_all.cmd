b6f56016d43fed3c7da35f26de4c4cfeb7359352
kimgrasman <kimgrasman@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2012-03-14 20:25:34 +0000

    Renamed build.cmd -> build32.cmd

--- dqsd/src/build_all.cmd
+++ dqsd/src/build_all.cmd
@@ -9,7 +9,7 @@ CALL scrub.cmd
 
 REM Build 32-bit first and 64-bit second, because the 64-bit 
 REM build process is smart enough to exclude all 32-bit artefacts
-CALL build.cmd
+CALL build32.cmd
 CALL build64.cmd
 
 POPD
