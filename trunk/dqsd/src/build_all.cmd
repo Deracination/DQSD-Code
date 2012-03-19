8341566609c09cc53dcce0058234ab3881fcf05d
kimgrasman <kimgrasman@5ae7b311-2a1c-0410-8cf6-a9e75a090f8d>
2012-03-19 20:36:24 +0000

    Error handling

--- trunk/dqsd/src/build_all.cmd
+++ trunk/dqsd/src/build_all.cmd
@@ -10,8 +10,16 @@ CALL scrub.cmd
 REM Build 32-bit first and 64-bit second, because the 64-bit 
 REM build process is smart enough to exclude all 32-bit artefacts
 CALL build32.cmd
+IF "%ERRORLEVEL%" NEQ "0" GOTO :FAIL
+
 CALL build64.cmd
+IF "%ERRORLEVEL%" NEQ "0" GOTO :FAIL
 
 POPD
 
 ENDLOCAL
+GOTO :EOF
+
+:FAIL
+@ECHO *** Build failed
+POPD
